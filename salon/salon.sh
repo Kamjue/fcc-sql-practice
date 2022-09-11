a392aa8dc3d5b59bbf0f9e81540c6400531c26a2
Daniel Lobo <Kamjue@users.noreply.github.com>
2022-09-10 23:16:40 -0400

    Update salon.sh to fixed version
    Previous one had a no exit bug

--- salon/salon.sh
+++ salon/salon.sh
@@ -16,16 +16,16 @@ MAIN_MENU() {
   read SERVICE_ID_SELECTED
 
   MAX_SERVICE_ID=$($PSQL "SELECT MAX(service_id) FROM services")
-  if [[ $SERVICE_ID_SELECTED =~ '^[0-9]+$' ]]
+
+  if [[ $SERVICE_ID_SELECTED > $MAX_SERVICE_ID || $SERVICE_ID_SELECTED -le 0 || $SERVICE_ID_SELECTED =~ '^[0-9]+$' ]]
   then
     MAIN_MENU "\nI could not find that service. What would you like today?"
+  else
+    CONTINUE_SALON
   fi
+}
 
-  if [[ $SERVICE_ID_SELECTED > $MAX_SERVICE_ID || $SERVICE_ID_SELECTED -le 0 ]]
-  then
-    MAIN_MENU "\nI could not find that service. What would you like today?"
-  fi
-
+CONTINUE_SALON () {
   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
 
   echo -e "\nWhat's your phone number?"
@@ -40,17 +40,18 @@ MAIN_MENU() {
     AUX=$($PSQL "INSERT INTO customers(phone,name) VALUES('$CUSTOMER_PHONE','$CUSTOMER_NAME')")
   fi
 
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    CUSTOMER_NAME=$RESULT_PHONE
+  fi
+
   echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
   read SERVICE_TIME
 
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
   AUX=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')")
 
-  echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-}
-
-EXIT() {
-  echo "Thank you."
+  echo -e "\nI have put you down for a$SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 MAIN_MENU
